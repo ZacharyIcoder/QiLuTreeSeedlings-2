84dadb22e7b984da9bfa6937bfafc5c49e24a5da
youngLD <yangledong@yangledongdeiMac.local>
2016-04-28 09:53:21 +0800

    手贱删代码

--- QiLuTreeSeedlings/Appdelegate/AppDelegate.m
+++ QiLuTreeSeedlings/Appdelegate/AppDelegate.m
@@ -370,31 +370,15 @@
     if (payload) {
         payloadMsg = [[NSString alloc] initWithBytes:payload.bytes length:payload.length encoding:NSUTF8StringEncoding];
     }
-    
-  //  NSString *msg = [NSString stringWithFormat:@" payloadId=%@,taskId=%@,messageId:%@,payloadMsg:%@%@", payloadId, taskId, aMsgId, payloadMsg, offLine ? @"<离线消息>" : @""];
-  //  NSLog(@"\n>>>[GexinSdk ReceivePayload]:%@\n\n", msg);
-    
-    /**
-     *汇报个推自定义事件
-     *actionId：用户自定义的actionid，int类型，取值90001-90999。
-     *taskId：下发任务的任务ID。
-     *msgId： 下发任务的消息ID。
-     *返回值：BOOL，YES表示该命令已经提交，NO表示该命令未提交成功。注：该结果不代表服务器收到该条命令
-     **/
     [GeTuiSdk sendFeedbackMessage:90001 taskId:taskId msgId:aMsgId];
 }
 
 /** SDK收到sendMessage消息回调 */
 - (void)GeTuiSdkDidSendMessage:(NSString *)messageId result:(int)result {
-    // [4-EXT]:发送上行消息结果反馈
-    //NSString *msg = [NSString stringWithFormat:@"sendmessage=%@,result=%d", messageId, result];
-  //  NSLog(@"\n>>>[GexinSdk DidSendMessage]:%@\n\n", msg);
 }
 
 /** SDK运行状态通知 */
 - (void)GeTuiSDkDidNotifySdkState:(SdkStatus)aStatus {
-    // [EXT]:通知SDK运行状态
-   // NSLog(@"\n>>>[GexinSdk SdkState]:%u\n\n", aStatus);
 }
 
 /** SDK设置推送模式回调 */
