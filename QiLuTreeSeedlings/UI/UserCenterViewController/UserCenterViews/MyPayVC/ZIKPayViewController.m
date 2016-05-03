66711e2200b1ea78d6aefac1c378224ae471008d
youngLD <15953523812@163.com>
2016-05-03 10:03:53 +0800

    充值更正

--- QiLuTreeSeedlings/UI/UserCenterViewController/UserCenterViews/MyPayVC/ZIKPayViewController.m
+++ QiLuTreeSeedlings/UI/UserCenterViewController/UserCenterViews/MyPayVC/ZIKPayViewController.m
@@ -86,11 +86,9 @@
 - (void)requestIsFirstRecharge {
     [HTTPCLIENT isFirstRecharge:nil Success:^(id responseObject) {
         if ([responseObject[@"success"] integerValue] == 1) {
-            if ([responseObject[@"result"]  integerValue] == 1) {
-                if (nameTextField.text.floatValue<100) {
-                    [ToastView showTopToast:@"第一次充值金额不能低于100元"];
-                    return;
-                }
+            if ([responseObject[@"result"]  integerValue] == 1 && nameTextField.text.floatValue<100) {
+                [ToastView showTopToast:@"第一次充值金额不能低于100元"];
+                return;
             }
             else {
                 ZIKVoucherCenterViewController *voucherVC = [[ZIKVoucherCenterViewController alloc] init];
