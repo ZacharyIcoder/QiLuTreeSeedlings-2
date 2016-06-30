be084dbc683f101b72e85127de3da83cbf244359
youngLD <yangledong@yangledongdeiMac.local>
2016-06-30 13:26:14 +0800

    同步

--- QiLuTreeSeedlings/UI/GongChengGongSiVVVVV/YLDhezuoxiangqingViewController/Model/YLDHeZuoDetial.m
+++ QiLuTreeSeedlings/UI/GongChengGongSiVVVVV/YLDhezuoxiangqingViewController/Model/YLDHeZuoDetial.m
@@ -12,7 +12,21 @@
 +(YLDHeZuoDetial *)creatYLDHeZuoDetialWithDic:(NSDictionary *)dic
 {
     YLDHeZuoDetial *model=[YLDHeZuoDetial new];
-    
+    model.area=[dic objectForKey:@"area"];
+    model.cooperateList=[dic objectForKey:@"cooperateList"];
+    model.dbh=[dic objectForKey:@"dbh"];
+    model.descriptions=[dic objectForKey:@"description"];
+    model.endDate=[dic objectForKey:@"endDate"];
+    model.groundDiameter=[dic objectForKey:@"groundDiameter"];
+    model.orderDate=[dic objectForKey:@"orderDate"];
+    model.orderName=[dic objectForKey:@"orderName"];
+    model.orderType=[dic objectForKey:@"orderType"];
+    model.person=[dic objectForKey:@"person"];
+    model.phone=[dic objectForKey:@"phone"];
+    model.quantityRequired=[dic objectForKey:@"quantityRequired"];
+    model.quotationRequired=[dic objectForKey:@"quotationRequired"];
+    model.status=[dic objectForKey:@"status"];
+    model.uid=[dic objectForKey:@"uid"];
     return model;
 }
 @end
